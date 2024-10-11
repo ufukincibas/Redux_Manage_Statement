17fe4758ccda21796489dd0d986031ba8489aaf6
ufukincibas <ufukincibas@hotmail.com>
2024-10-11 22:51:53 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from "react";
+import {View , Text, Button} from "react-native";
+import {legacy_createStore as createStore} from 'redux';
+import { Provider , useSelector , useDispatch } from "react-redux";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const initialState ={
+  counter:0, 
+}
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function reducers(state , action){
+  switch(action.type){
+    case 'updateCounter' :
+      return {...state , counter: state.counter + 1};
+      
+      default:
+        return  state;
+  }
+}
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function App(){
+  return(
+    <Provider store={createStore(reducers,initialState)}>
+<View style={{flex:1}}>
+  <First/>
+  <Second/>
+  </View>
+  </Provider>
+  )
+}
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+
+
+
+function First(){
+  const counter = useSelector(selector => selector.counter) //use
+  const dispatch = useDispatch()
+  function handleUpdate(){
+    dispatch({type: 'updateCounter'})
+
+  }
+  return(
+    <View style={{flex:1}}>
+      <Text>First : {counter}</Text>
+      <Button title="Update" onPress={handleUpdate}/>
+      </View>
+  );
+}
+
+function Second(){
+  const counter = useSelector(selector => selector.counter)
+  
+  return(
+    <View style={{backgroundColor:'gray', flex:1}}>
+      <Text>Second : {counter}</Text>
     </View>
-  );
+  )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
